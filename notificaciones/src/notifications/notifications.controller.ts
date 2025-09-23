1ee80536272b07c9721080e364b2cf56f2cfddc7
jhoaking <peredoroj@gmail.com>
2025-09-23 14:34:49 -0400

    praparando para probar sse

--- notificaciones/src/notifications/notifications.controller.ts
+++ notificaciones/src/notifications/notifications.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { CreateNotificationDto } from './dto/create-notification.dto';
 import { UpdateNotificationDto } from './dto/update-notification.dto';
@@ -18,8 +18,8 @@ export class NotificationsController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.notificationsService.findOne(+id);
+  findOne(@Param('id' ,ParseUUIDPipe) id: string) {
+    return this.notificationsService.findOne(id);
   }
 
   @Patch(':id')
--- notificaciones/src/notifications/notifications.service.ts
+++ notificaciones/src/notifications/notifications.service.ts
@@ -1,19 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateNotificationDto } from './dto/create-notification.dto';
 import { UpdateNotificationDto } from './dto/update-notification.dto';
+import { Notification } from './entities/notification.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class NotificationsService {
-  create(createNotificationDto: CreateNotificationDto) {
-    return 'This action adds a new notification';
+  constructor(
+    @InjectRepository(Notification)
+    private readonly notificationRepository: Repository<Notification>,
+  ) {}
+
+  async create(createNotificationDto: CreateNotificationDto) {
+    try {
+      const notification = this.notificationRepository.create(
+        createNotificationDto,
+      );
+      await this.notificationRepository.save(notification);
+      return notification;
+    } catch (error) {
+      throw new InternalServerErrorException(`error with to db`);
+    }
   }
 
   findAll() {
     return `This action returns all notifications`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} notification`;
+  async findOne(id: string) {
+    return await this.notificationRepository.findOneBy({ id });
   }
 
   update(id: number, updateNotificationDto: UpdateNotificationDto) {
