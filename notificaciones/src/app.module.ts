79a7c4457ade278f3afb19be4caa802a712bff1d
jhoaking <peredoroj@gmail.com>
2025-09-24 10:03:30 -0400

    levantamiento con docker mas coenxion a db

--- notificaciones/src/app.module.ts
+++ notificaciones/src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { NotificationsModule } from './notifications/notifications.module';
+import {ConfigModule} from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [NotificationsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type : 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT!,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    NotificationsModule],
   controllers: [],
   providers: [],
 })
